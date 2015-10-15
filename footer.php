e2c2d837fe9a7c17e0fae14f70cb6ec2ba02e808
Don Nelson <don@linkedwebdesign.com>
2015-10-14 20:18:33 -0500

    Made site title dynamic

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 			<a href="https:www.facebook.com" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a>
 			<a href="https://twitter.com" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a>
 			<a href="https://www.pinterest.com" target="_blank"><i class="fa fa-pinterest-square fa-2x"></i></a>
-			<p>Copyright &copy; <?php echo date('Y'); ?> Incredible Destinations Travel, LLC</p>
+			<p>Copyright &copy; <?php echo date('Y') . " " . $blog_title = get_bloginfo(); ?></p>
 		</div>
 	</footer>
 
