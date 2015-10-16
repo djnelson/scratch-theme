ba910a94b03374ca6214202bcca4fb32cbdcc815
Don Nelson <don@linkedwebdesign.com>
2015-10-16 15:08:37 -0500

    Removed hard coded custom .js file

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 	<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="_js/site-behaviors.js"></script>
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
