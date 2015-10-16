0dfdbb1a0b99b4236cdc431cad902025d195bd02
Don Nelson <don@linkedwebdesign.com>
2015-10-16 11:47:44 -0500

    Replaced widgets with WP Loop

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
 
-<div class="section-group">
-    <?php dynamic_sidebar( 'promo1' ); ?>
-    <?php dynamic_sidebar( 'promo2' ); ?>
-    <?php dynamic_sidebar( 'promo3' ); ?>
-</div>
+<!-- Begin WP Loop -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php endwhile; else : ?>
+		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+<!-- End WP Loop -->
 
 <?php get_footer(); ?>
