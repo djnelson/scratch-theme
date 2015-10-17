167047907f058583bc792cd84a4e4a736e06f58d
Don Nelson <don@linkedwebdesign.com>
2015-10-16 19:51:10 -0500

    Added comments

--- page.php
+++ page.php
@@ -1,17 +1,18 @@
 <?php get_header(); ?>
 
+<!-- Begin WP Loop -->
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<div class="section-group">
 		<div class="col span_12_of_12">
-			<?php get_template_part( 'content', 'page' );?>
 			<?php the_title( '<h1 class="pageTitle">', '</h1>' ); ?>
 			<?php the_content( '<p class="pageContent">', '</p>' ); ?>
-	    </div>
+	  </div>
 	</div>
 
 <?php endwhile; else : ?>
 		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
+<!-- End WP Loop -->
 
 <?php get_footer(); ?>
