06d9ab4a9f4683c7fee3c1bd1f5e4c52e1b9e42c
Don Nelson <don@linkedwebdesign.com>
2015-10-17 22:03:39 -0500

    Corrected WP Loop

--- home.php
+++ home.php
@@ -1,4 +1,7 @@
-<$php get_header(); ?>
+<?php get_header(); ?>
+
+<!-- Begin WP Loop -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php endwhile; else : ?>
 		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
